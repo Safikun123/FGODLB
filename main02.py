4aff9599c6222a9954d79fac28d3e62742d45e46
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-05-07 22:44:36 -0600

    Update main02.py

--- main02.py
+++ main02.py
@@ -6,20 +6,19 @@ import fgourl
 import user
 import coloredlogs
 import logging
-from datetime import datetime  # Don't forget to import datetime!
+from datetime import datetime
 
-# Environment Variables
+# Environments Variables
 userIds = os.environ['userIds'].split(',')
 authKeys = os.environ['authKeys'].split(',')
 secretKeys = os.environ['secretKeys'].split(',')
 fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
 blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
+idempotency_key_signature = os.environ.get('IDEMPOTENCY_KEY_SIGNATURE_SECRET')
+device_info = os.environ.get('DEVICE_INFO_SECRET')
+user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
 
-UA = os.environ['UserAgent']
-
-if UA:
-    fgourl.user_agent_ = UA
 
 userNums = len(userIds)
 authKeyNums = len(authKeys)
@@ -31,18 +30,24 @@ coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 def check_blue_apple_cron(instance):
     if blue_apple_cron:
+
         cron = croniter(blue_apple_cron)
         next_date = cron.get_next(datetime)
         current_date = datetime.now()
-
+        
         if current_date >= next_date:
-            logger.info('Exchanging Blue Fruit!')
+            logger.info('Trying buy one blue apple!')
             instance.buyBlueApple(1)
             time.sleep(2)
 
 
 def get_latest_verCode():
-    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-VerCode-extractor/JP/VerCode.json"
+    endpoint = ""
+
+    if fate_region == "NA":
+        endpoint += "https://raw.githubusercontent.com/O-Isaac/FGO-VerCode-extractor/NA/VerCode.json"
+    else:
+        endpoint += "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
 
     response = requests.get(endpoint).text
     response_data = json.loads(response)
@@ -52,27 +57,28 @@ def get_latest_verCode():
 
 def main():
     if userNums == authKeyNums and userNums == secretKeyNums:
-        logger.info('Fetching Game Data')
         fgourl.set_latest_assets()
 
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info('Login in...')
-                instance.topLogin2()
+                logger.info('登录账号!')
+                instance.SignedData()
+                time.sleep(1)
+                instance.topLogin_s()
                 time.sleep(2)
                 instance.topHome()
                 time.sleep(2)
                 try:
-                    time.sleep(2)
-                    logger.info('Pulling FP Summon!')
-                    for _ in range(1):  # Define how many times to summon friend points (default 1 time)
-                        instance.drawFP()
-                        time.sleep(4)
+                   time.sleep(2)
+                   logger.info('pulling fp summon!!')
+                   for _ in range(1): #可定义每次登录时自动抽几次友情10连（默认1次） 
+                      instance.drawFP()
+                      time.sleep(4)
                 except Exception as ex:
                     logger.error(ex)
-
+                    
             except Exception as ex:
                 logger.error(ex)
 
